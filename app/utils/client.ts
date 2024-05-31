e26d15debb37e2ec72a9e8c6fad03f7527980a3a
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-05-31 19:31:54 +0530

    feat: added clients

---
+++ app/utils/client.ts
@@ -0,0 +1,47 @@
+import { createPublicClient, http, fallback, createWalletClient } from "viem";
+import { arbitrumSepolia, sepolia, mainnet, arbitrum } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
+// import { Alchemy, Network } from "alchemy-sdk";
+
+const sepoliaRpc = process.env.SEPOLIA_RPC;
+const arbitrumSepoliaRpc = process.env.ARBITRUM_SEPOLIA_RPC;
+const mainnetRpc = process.env.MAINNET_RPC;
+const arbitrumRpc = process.env.ARBITRUM_RPC;
+const account = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
+
+export const sepoliaClient = createPublicClient({
+  chain: sepolia,
+  transport: fallback([
+    http(sepoliaRpc, {
+      batch: true,
+    }),
+    http("https://eth-sepolia.public.blastapi.io"),
+  ]),
+});
+
+export const arbitrumSepoliaClient = createPublicClient({
+  chain: arbitrumSepolia,
+  transport: http(arbitrumSepoliaRpc, {
+    batch: true,
+  }),
+});
+
+export const mainnetClient = createPublicClient({
+  chain: mainnet,
+  transport: fallback([
+    http(mainnetRpc, {
+      batch: true,
+    }),
+    http("https://eth-mainnet.public.blastapi.io"),
+  ]),
+});
+
+export const arbitrumClient = createPublicClient({
+  chain: arbitrum,
+  transport: fallback([
+    http(arbitrumRpc, {
+      batch: true,
+    }),
+    http("https://eth-arbitrum.public.blastapi.io"),
+  ]),
+});
